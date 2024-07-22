be840558adc208a8a9a1f5468906a0e911eea7a0
Yusuke Wada <yusuke@kamawada.com>
2024-07-22 18:44:43 +0900

    use the latest `hono`

--- sw.ts
+++ sw.ts
@@ -1,7 +1,3 @@
-// Support types
-// https://github.com/microsoft/TypeScript/issues/14877
-declare const self: ServiceWorkerGlobalScope
-
 import { Hono } from 'hono'
 import { handle } from 'hono/service-worker'
 
